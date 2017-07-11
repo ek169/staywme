257332468f1cc39599ef7048d776bf149f3c4bd2
ethankulman <ethan@Ethans-MacBook-Pro.local>
2017-07-10 22:46:04 -0400

    back to original

--- staywme/settings.py
+++ staywme/settings.py
@@ -131,7 +131,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend', 'build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend')
 
 STATICFILES_DIRS = [
 
