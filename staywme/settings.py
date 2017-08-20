2463a3372349db0e0b1e18cbdd86fc196a2c8d4f
ethankulman <ethan@Ethans-MacBook-Pro.local>
2017-08-20 15:00:19 -0400

    accidently added a line in settings

--- staywme/settings.py
+++ staywme/settings.py
@@ -17,7 +17,6 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-heroku run python manage.py migrate auth
 
 
 # Quick-start development settings - unsuitable for production
